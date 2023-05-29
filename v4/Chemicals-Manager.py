19096391ea44f5dcfc0ff66861d06d78ce2bad70
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-05-29 23:53:43 +0300

    general sync

--- v4/Chemicals-Manager.py
+++ v4/Chemicals-Manager.py
@@ -5,32 +5,68 @@ class main:
         # load smart console
         self.sc = SmartConsole("Chemical Manager", "4.0")
 
-        # settings / paths
-        self.path_main_folder = self.sc.load_path("Chemicals folder")
-        self.path_stockcount = self.sc.load_path(self.path_main_folder+"/Stockcount.txt")
-        self.path_yellow_label = self.sc.load_path(self.path_main_folder+"/LBL Chemical Closet AR00162 Yellow Label.btw")
-        self.path_yellow_label_csv = self.sc.load_path(self.path_main_folder+"/YellowLabel.csv")
+        # set-up main memu
+        self.sc.main_menu["ADD NEW LOT"] = self.new_lot
+        self.sc.main_menu["ADD NEW PART NUMBER"] = self.new_part_number
+        self.sc.main_menu["PRINT NEW YELLOW LABEL"] = self.new_yellow_label
+        self.sc.main_menu["DO STOCK-COUNT"] = self.stockcount
+        self.sc.main_menu["GENERATE HTML REPORTS"] = self.generate_html_report
 
-        # main menu
-        self.sc.set_main_menu(main_menu)
+        # get settings
+        self.path_main = self.sc.get_setting("Chemicals folder")
+        self.path_stockcount = self.path_main+"/Stockcount.txt"
+        self.path_yellow_lbl = self.path_main+"/LBL Chemical Closet AR00162 Yellow Label.btw"
+        self.path_yellow_lbl_csv = self.path_main+"/YellowLabel.csv"
+        self.path_lots_csv = self.path_main+"/Lots.csv"
+        self.path_chemicals_csv = self.path_main+"/Chemicals.csv"
+
+        # test all paths
+        self.sc.test_path(self.path_main)
+        self.sc.test_path(self.path_yellow_lbl)
+        self.sc.test_path(self.path_yellow_lbl_csv)
 
         # load databases
-        self.CHEMICALS = self.sc.load_database()
-        self.LOTS = self.sc.load_database()
-        self.BOXES = self.sc.load_database()
+        self.load_databases()
 
         # display main menu NEW LOT, NEW PART NUMBER, NEW YELLOW LABEL, STOCK COUNT, GENERATE HTML REPORT
-        self.sc.display_main_menu()
+        self.sc.start()
+    
+    def load_databases(self):
+        # load databases
+        self.CHEMICALS = self.sc.load_database(self.path_chemicals_csv, ("PART NUMBER","DESCRIPTION","STORAGE CONDITIONS","NICKNAME","FRIDGE","MSDS"))
+        self.LOTS = self.sc.load_database(self.path_lots_csv, ("LOT NUMBER","PART NUMBER","EXPIRATION DATE"))
+        self.BOXES = self.sc.load_database(self.path_stockcount, ("BOXID",))
 
     def new_lot(self):
         # get lot number
+        lot_number = self.sc.input("Insert new LOT NUMBER")
+
         # make sure lot is not new
-        # get part number
-        # make sure part number exists
-        # get exp date
+        if not lot_number in self.LOTS:
+            # get part number
+            part_number = self.sc.input("Insert PART NUMBER")
+            # make sure part number exists
+            if not part_number in self.CHEMICALS:
+                self.sc.error("this PART NUMBER is not in the database")
+                self.sc.restart()
+                return
+            else:
+                # get exp date
+                exp = self.sc.input("Insert EXPIRATION DATE")
+                if self.sc.compare_dates(exp, self.sc.today()) <= 0:
+                    self.sc.error("This date invalid or expired!")
+                    self.sc.restart()
+                    return
+                else:
+                    self.LOTS[lot_number] = [part_number, exp]
+                    self.sc.save_database(self.path_lots_csv ,self.LOTS)
+                    self.sc.restart()
+        else:
+            self.sc.error("LOT NUMBER is not new")
+            self.sc.restart()
+            return
         # validate exp
         # restart
-        pass
 
     def new_part_number(self):
         # get pn
--- v4/SmartConsole.py
+++ v4/SmartConsole.py
@@ -1,13 +1,12 @@
 import os
 import sys
-
+import datetime
+from datetime import date as date_n
+import re
 class SmartConsole:
     def __init__(self, name, version):
-        # display title
-        title = name+" v"+version
-        print("---"+"-"*len(title)+"---")
-        print("-- "+title+" --")
-        print("---"+"-"*len(title)+"---")
+        self.title = name+" v"+version
+        self.main_menu = {}
 
         # load settings.txt
         if not os.path.isfile("settings.txt"):
@@ -22,21 +21,168 @@ class SmartConsole:
                 if ">" in line:
                     line = line.split(">")
                     if len(line) == 2:
-                        self.__loaded_settings[line[0]] = line[1]
-    
-    def load_path(self, pathname):
-        if pathname in self.__loaded_settings:
-            path = self.__loaded_settings[pathname]
-            if not os.path.isdir(path) and not os.path.isfile(path):
-                self.fatal_error("Missing path: "+path)
-            else:
-                return path
+                        self.__loaded_settings[line[0].strip()] = line[1].strip()
+        
+    def start(self):
+        # clear console
+        os.system('cls')
+
+        # display title
+        print("---"+"-"*len(self.title)+"---")
+        print("-- "+self.title+" --")
+        print("---"+"-"*len(self.title)+"---")
+
+        # display main menu
+        self.main_menu["SETTINGS"] = self.open_settings
+        self.main_menu["HELP"] = self.help
+        self.main_menu["EXIT"] = self.exit
+        print("MAIN MENU:")
+        item = 0
+        options = {}
+        for key, value in self.main_menu.items():
+            item += 1
+            print(str(item)+". "+key)
+            options[str(item)] = value
+        
+        # get input
+        ans = self.input("Insert your choice")
+        if ans in options:
+            options[ans]()
         else:
-            self.fatal_error("Missing settings: "+pathname)
+            self.error("Invalid selection")
+            self.restart()
+            return
+
+    def restart(self):
+        self.input("Press ENTER to restart")
+        self.start()
     
+    def error(self, text):
+        print("ERROR "+text)
+
+    def test_path(self, path):
+        if not os.path.isdir(path) and not os.path.isfile(path):
+            self.fatal_error("Missing path: "+path)
+    
+    def get_setting(self, var):
+        if var in self.__loaded_settings:
+            return self.__loaded_settings[var]
+        else:
+            self.fatal_error("Missing setting: "+var)
+
     def fatal_error(self, text):
         print("ERROR! "+text)
         self.exit()
     
+    def input(self, text):
+        return input(text+" >")
+    
     def exit(self):
+        self.input("Press ENTER to exit")
+        os._exit(1)
         sys.exit()
+
+    def open_settings(self):
+        os.popen("settings.txt")
+
+    def help(self):
+        os.popen("help.pdf")
+
+    # DATABASES
+    def save_database(self, path, data):
+        self.test_path(path)
+        file = open(path, 'w')
+        for key, values in data.items():
+            valstowrite = ""
+            for val in values:
+                valstowrite += val+","
+            valstowrite = valstowrite[:-1]
+            file.write(key+","+valstowrite+"\n")
+        file.close()
+
+    def load_database(self, path, headers):
+        return_value = {}
+        self.test_path(path)
+        file = open(path, 'r')
+        lines = file.readlines()
+        file.close()
+        ln = 0
+        if len(lines) > 0:
+            for line in lines:
+                ln += 1
+                line = line.replace("\n", "")
+                line = line.split(",")
+                if len(line) != len(headers):
+                    self.fatal_error("in file: "+path+"\nIn line #"+str(ln)+"\nIncorrect number of values")
+                else:
+                    if ln == 1:
+                        i = 0
+                        for h in line:
+                            if h != headers[i]:
+                                self.fatal_error("in file: "+path+"\nIn line #"+str(ln)+"\nInvalid header\nGiven: '"+h+"' Expected: '"+headers[i]+"'")
+                            i += 1
+                    else:
+                        key = line[0]
+                        values = line[1:]
+                        if not key in return_value:
+                            return_value[key] = values
+                        else:
+                            self.fatal_error("in file: "+path+"\nIn line #"+str(ln)+"\nPrimary key: "+key+" is not unique")
+        else:
+            ln += 1
+            self.fatal_error("in file: "+path+"\nIn line #"+str(ln)+"\nIncorrect number of values")
+        return return_value
+
+    # DATE
+    def today(self):
+        # get today
+        now = datetime.datetime.now()
+        yyyy = str(now.year)
+        mm = str(now.month).zfill(2)
+        dd = str(now.day).zfill(2)
+        return yyyy+"-"+mm+"-"+dd
+    
+    def test_date(self, givenDate):
+        # make sure the format is correct
+        tmp = givenDate.replace("-","")
+        tmp = re.sub('[^0-9]','', tmp)
+        if len(tmp) < 8:
+            self.error("Invalid date format: "+givenDate)
+            return False
+        
+        # test year format
+        YYYY = int(tmp[0:4])
+        if not YYYY in range(0,10000):
+            self.error("Invalid year format: "+str(YYYY)+" acceptable range 0-9999")
+            return False
+        YYYY = str(YYYY)
+        YYYY = YYYY.zfill(4)
+
+        # test month format
+        MM = int(tmp[4:6])
+        if not MM in range(1,13):
+            self.error("Invalid month format: "+str(MM)+" acceptable range 1-12")
+            return False
+        MM = str(MM)
+        MM = MM.zfill(2)
+
+        # test day format
+        DD = int(tmp[6:8])
+        if not DD in range(1,32):
+            self.error("Invalid day format: "+str(DD)+" acceptable range 1-31")
+            return False
+        DD = str(DD)
+        DD = DD.zfill(2)
+        
+        # return true if date is good
+        return True
+
+    def compare_dates(self, firstDate, secondDate):
+        if self.test_date(firstDate) and self.test_date(secondDate):
+            firstDate = firstDate.replace("-","")
+            secondDate = secondDate.replace("-","")
+            date1 = date_n(int(firstDate[0:4]), int(firstDate[4:6]), int(firstDate[6:8]))
+            date2 = date_n(int(secondDate[0:4]), int(secondDate[4:6]), int(secondDate[6:8]))
+            return (date1 - date2).days
+        else:
+            return 0
